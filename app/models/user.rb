cf8df501b7afc38c9db3756610f549e741251c60
KaFaiC <choikafai2004@gmail.com>
2014-07-08 19:25:48 +0800

    Add OmniAuth facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.username = auth.info.name   # assuming the user model has a name
+      user.remote_image_url = auth.info.image.gsub('http://','https://') # assuming the user model has an image
     end
   end
 
